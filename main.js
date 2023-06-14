360cfa526094fa6e26097f779dbbaa4e62e2f133
chuckdamasco <gdamasco@gmail.com>
2023-06-14 15:51:42 -0300

    Adjustments

--- main.js
+++ main.js
@@ -1,33 +1,41 @@
+//variáveis
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
-
-
-const randomNumber = Math.round(Math.random() * 10)
+let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
+//Eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+
+//Função Callback
 function handleTryClick(event) {
     event.preventDefault()
 
     const inputNumber = document.querySelector("#inputNumber")
 
     if(Number(inputNumber.value) == randomNumber) {
-        screen1.classList.add("hide")
-        screen2.classList.remove("hide")
+        toggleScreen()
 
-        document.querySelector(".screen2 h2").innerText = `Você acertou na ${xAttempts}ª tentativa!`
+        screen2.querySelector("h2").innerText = `Você acertou na ${xAttempts}ª tentativa!`
+    }
+
+    if (Number(inputNumber.value)<0|| Number(inputNumber.value)>10|| (!Number(inputNumber.value) && Number(inputNumber.value)!=0)){
+        alert('Por favor, insira um número de 1 a 10')
     }
     inputNumber.value = ""
     xAttempts++
-
-   
 }
 
-const btnTry = document.querySelector("#btnTry")
-const btnReset = document.querySelector("#btnReset")
+function handleResetClick() {
+    toggleScreen()
+    xAttempts = 1
+    randomNumber = Math.round(Math.random() * 10)
+}
 
-btnTry.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', function() {
-        screen1.classList.remove("hide")
-        screen2.classList.add("hide")
-        xAttempts = 1
-})
+function toggleScreen() {
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
+}
